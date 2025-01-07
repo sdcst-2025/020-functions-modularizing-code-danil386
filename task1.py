989145f9fb9c9ef08bd5491cf8988a38a2aca24a
danil386 <danielal386@deltalearns.ca>
2025-01-07 11:05:35 -0800

    finished

--- task1.py
+++ task1.py
@@ -1,6 +1,17 @@
 def A():
+    print("Hello")
 
 def B():
-
+    print("How are you")
 
 def C():
+    print("Hi")
+
+x = input("Enter a letter from A to C: ")
+
+if x == "A":
+    A()
+elif x == "B":
+    B()
+elif x == "C":
+    C()
--- task2.py
+++ task2.py
@@ -8,3 +8,25 @@ displays instructions and how to play
 game()
 plays the games
 """
+
+import random
+
+def title():
+    print("Rock, Paper, Scissors")
+    print("Instructions: When prompted, enter either rock, paper, or scissors. Rock beats scissors, paper beats rock, and scissors beats paper. The goal of the game is to beat the computer, who will make a random choice between the three every time.")
+
+def game():
+    options = ["rock", "paper", "scissors"]
+    pchoice = input("Enter rock, paper or scissors: ")
+    cchoice = random.choice(options)
+    print(f"The computer chose {cchoice}")
+    if (pchoice == "scissors" and cchoice == "rock") or (pchoice == "rock" and cchoice == "paper") or (pchoice == "paper" and cchoice == "scissors"):
+        print("You Lost")
+    elif pchoice == cchoice:
+        print("Draw")
+    else:
+        print("You win")
+
+
+title()
+game()
